8139283bfb709fff53b3b603e07c13a8e065c2e8
Ahmed Loay <ahmed@shahwan.me>
2019-01-29 21:06:29 +0200

    user dashboard
    redirect user to dashboard after login + other necessary code

--- account/urls.py
+++ account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
