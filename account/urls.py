28a745fa9e3c19689fd201af704140d5690cbd44
Ahmed Loay <ahmed@shahwan.me>
2019-01-30 12:34:51 +0200

    change password

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,14 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # change password urls
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change'),
+    path(
+        'password_change/done',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'),
     path('', views.dashboard, name='dashboard'),
 ]
