6ec7c9a2ac3dd44ffc9e774be4b5c3ab82d5ba87
Ahmed Loay <ahmed@shahwan.me>
2019-01-31 13:07:38 +0200

    user account registration
    created a UserRegistrationForm, then used it in register view, then added the corresponding template in register.html & register_done.html

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # include auth all url paths instead of defining them manually
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
     path('', views.dashboard, name='dashboard'),
 ]
