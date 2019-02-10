ff740d7584903250b4e7e3fb1b186ca0f7c54f93
Ahmed Loay <ahmed@shahwan.me>
2019-02-11 00:23:44 +0200

    image ranking with redis

--- images/urls.py
+++ images/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
     path('like/', views.image_like, name='like'),
+    path('ranking/', views.image_ranking, name='ranking'),
     path('', views.image_list, name='list'),
 ]
--- images/views.py
+++ images/views.py
@@ -39,6 +39,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1
     total_views = r.incr(f'image:{image.id}:views')
+    # increment image ranking by 1
+    r.zincrby('image_ranking', image.id, 1)
     return render(
         request, 'images/image/detail.html', {'section': 'images', 'image': image, 'total_views': total_views})
 
@@ -82,3 +84,14 @@ def image_list(request):
     if request.is_ajax():
         return render(request, 'images/image/list_ajax.html', {'section': 'images', 'images': images})
     return render(request, 'images/image/list.html', {'section': 'images', 'images': images})
+
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request, 'images/image/ranking.html', {'section': 'images', 'most_viewed': most_viewed})
